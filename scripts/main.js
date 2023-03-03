4f18d6d66686bb3424f5750dac38e380224135ad
icvitanovic <ivan.cvitanovic93@gmail.com>
2023-03-03 17:56:31 +0100

    responsive fixes

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
             height: '90vh',
             cover: true,
             heightRatio: 0.5,
+            breakpoints: {
+                640: {
+                    height: '60vh',
+                },
+                1023: {
+                    height: '60vh',
+                }
+            }
         });
     splide.mount();
 });
