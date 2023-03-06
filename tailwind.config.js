bbf9346d345caf3f3c8286028cb1d878e0a142e5
ancora-camping <126723809+ancora-camping@users.noreply.github.com>
2023-03-06 12:34:11 +0000

    Contact form

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,27 @@ module.exports = {
       },
       colors: {
         primary: '#03497E',
-        secondary: '#0596D5',
-        accent: '#FEB228'
+        secondary: {
+          500: '#69c0e5',
+          600: '#50b5e1',
+          700: '#36abdd',
+          800: '#1da0d9',
+          900: '#0596d5',
+          1000: '#0487bf',
+        },
+        accent: {
+          50: '#fef7e9',
+          100: '#feefd4',
+          200: '#fee7be',
+          300: '#fee0a9',
+          400: '#fed893',
+          500: '#fed07e',
+          600: '#fec968',
+          700: '#fec152',
+          800: '#feb93d',
+          900: '#FEB228',
+          1000: '#e4a024',
+        },
       },
       screens: {
         '3xl': '1600px',
